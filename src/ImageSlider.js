fb3f1f67f61d9ac795dea3c89812359bbcd46831
Shyam3050 <nimmanashyam3050@gmail.com>
2023-11-30 20:54:42 +0530

    autoscroll

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,12 +1,6 @@
+import { useEffect } from "react";
 import { useState } from "react";
-
-const data = [
-  "https://c4.wallpaperflare.com/wallpaper/86/419/788/random-green-hd-wallpaper-preview.jpg",
-  "https://e1.pxfuel.com/desktop-wallpaper/10/420/desktop-wallpaper-abstract-paint-splatter-style-blobs-cartoon-motion-backgrounds-blue-splatter-background.jpg",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThic2tKpmXMu8vMaXdt_S_nE0FGrbhPjDtR6ZL9idY8ByijG48sFbwvprf2TU42bCOLGQ&usqp=CAU",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD7KjdeW_cIwgUdmKTdQHdNzSOC_Fqs3U2Pnes1YvU0w2bKYz-nAYLyuypN4-qkqQXYwk&usqp=CAU",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhUxeU6FvIWymAshd0vbaRBVGjFrOy6WmB3g&usqp=CAU",
-];
+import {data} from "../src/constant"
 
 const ImageSlider = () => {
   const [activeImgIndex, setActiveImgIndex] = useState(0);
@@ -18,6 +12,13 @@ const ImageSlider = () => {
   function nextHandler() {
     setActiveImgIndex((activeImgIndex + 1) % data.length);
   }
+
+  useEffect(() =>{
+   const timer = setTimeout(() => {nextHandler()},5000)
+   return () =>{
+    clearTimeout(timer)
+   }
+  }, [activeImgIndex])
   return (
     <div className="flex justify-center">
       <button className="border mr-6 bg-gray-100" onClick={prevHandler}>
---
+++ src/constant.js
@@ -0,0 +1,8 @@
+export const data = [
+    "https://c4.wallpaperflare.com/wallpaper/86/419/788/random-green-hd-wallpaper-preview.jpg",
+    "https://e1.pxfuel.com/desktop-wallpaper/10/420/desktop-wallpaper-abstract-paint-splatter-style-blobs-cartoon-motion-backgrounds-blue-splatter-background.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThic2tKpmXMu8vMaXdt_S_nE0FGrbhPjDtR6ZL9idY8ByijG48sFbwvprf2TU42bCOLGQ&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD7KjdeW_cIwgUdmKTdQHdNzSOC_Fqs3U2Pnes1YvU0w2bKYz-nAYLyuypN4-qkqQXYwk&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhUxeU6FvIWymAshd0vbaRBVGjFrOy6WmB3g&usqp=CAU",
+  ];
+  
